7d12b8bf3959631fa456b447c173218214f3d8f3
Nastia <nastasia.schull@gmail.com>
2024-12-28 15:20:24 +0100

    Added animation system for animals.

---
+++ Assets/Scripts/PatrolBetweenPoints.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PatrolBetweenPoints : MonoBehaviour
+{
+    [SerializeField] private List<Transform> checkpoints; 
+    [SerializeField] private float waitTimeAtCheckpoint = 6f; 
+    private NavMeshAgent agent;
+    private int currentCheckpointIndex = 0;
+    private bool waiting = false;
+    private Animator animator;
+
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>(); 
+
+        if (checkpoints == null || checkpoints.Count == 0)
+        {
+            return;
+        }
+
+        MoveToNextCheckpoint();
+    }
+
+
+    private void Update()
+
+    {
+        // анимацию синхронизируем с движением
+        float speed = agent.velocity.magnitude;
+        animator.SetFloat("Speed", speed); 
+
+        if (agent.remainingDistance <= agent.stoppingDistance && !waiting)
+        {
+            Debug.Log("Reached checkpoint, waiting...");
+            StartCoroutine(WaitAndMoveToNextCheckpoint());
+        }
+    }
+
+
+    private IEnumerator WaitAndMoveToNextCheckpoint()
+    {
+        waiting = true;
+        animator.SetBool("isIdle", true); 
+        yield return new WaitForSeconds(waitTimeAtCheckpoint);
+        animator.SetBool("isIdle", false); 
+        waiting = false;
+
+        MoveToNextCheckpoint();
+    }
+
+    private void MoveToNextCheckpoint()
+    {
+        if (checkpoints.Count == 0) return;
+
+        Debug.Log($"Moving to checkpoint: {checkpoints[currentCheckpointIndex].name}");
+        agent.SetDestination(checkpoints[currentCheckpointIndex].position);
+
+        currentCheckpointIndex = (currentCheckpointIndex + 1) % checkpoints.Count;
+    }
+
+}
